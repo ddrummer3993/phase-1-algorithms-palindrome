104aef1280d5ca6665ca01b6fee1c0413ff43cbc
David Drummer <david.b.drummer@gmail.com>
2022-03-02 14:14:37 -0500

    commit tree 64e74495f2556b2b7f72962341575d504733f17a

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  let wordReverse = word.split('').reverse().join('');
+  if (word === wordReverse) {
+    return true;
+  } else {
+    return false;
+  };
+};
 
 /* 
   Add your pseudocode here
+
+  create function with string input
+    take string, split it up, reverse it, and join it back together. name this as a variable.
+    compare string input vs reversed string 
+      if same, return true
+      if not the same, return false
 */
 
 /*
   Add written explanation of your solution here
+
+  a function receives a string input and determines if the string is the exact same forwards as it is backwards.
+  if it is, the function will return true because that word or phrase is a palindrome.
+  if it is not, the function will return false. aka not a palindrome
 */
 
 // You can run `node index.js` to view these console logs
@@ -16,10 +31,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("radar"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("steve"));
 }
 
 module.exports = isPalindrome;
